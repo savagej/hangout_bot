a684581953a18691abbfa20ebc5248181a4390a3
John Savage <savagej@uchicago.edu>
2015-01-29 22:09:52 -0600

    Put all javascript into js/create_chatbot.js then made function to extract email addresses from file. It them makes a form for you to select your email addresses.

--- js/create_chatbot.js
+++ js/create_chatbot.js
@@ -1,12 +1,97 @@
-var main = function () {
-    // Check for the various File API support.
-    if (window.File && window.FileReader && window.FileList && window.Blob) {
-      //alert('Great success! All the File APIs are supported.');
-    } else {
-      alert('The File APIs are not fully supported in this browser.');
-    }
-    $('.status-box').keypress(function(e) {
-    });
-};
+var reader;
+var lines;
+console.log('loaded');
 
-$(document).ready(main);
+function abortRead() {
+  reader.abort();
+}
+
+function errorHandler(evt) {
+  switch(evt.target.error.code) {
+    case evt.target.error.NOT_FOUND_ERR:
+      alert('File Not Found!');
+      break;
+    case evt.target.error.NOT_READABLE_ERR:
+      alert('File is not readable');
+      break;
+    case evt.target.error.ABORT_ERR:
+      break; // noop
+    default:
+      alert('An error occurred reading this file.');
+  };
+}
+
+function updateProgress(evt) {
+  // evt is an ProgressEvent.
+  if (evt.lengthComputable) {
+    var percentLoaded = Math.round((evt.loaded / evt.total) * 100);
+    // Increase the progress bar length.
+    if (percentLoaded < 100) {
+      $('.progress-bar').css('width', percentLoaded + '%').attr('aria-valuenow', percentLoaded);
+    }
+  }
+}
+
+function handleFileSelect(evt) {
+  // Reset progress indicator on new file selection.
+  //progress.style.width = '0%';
+  //progress.textContent = '0%';
+
+  reader = new FileReader();
+  reader.onerror = errorHandler;
+  reader.onprogress = updateProgress;
+  reader.onabort = function(e) {
+    alert('File read cancelled');
+  };
+  reader.onloadstart = function(e) {
+    $('.progress-bar').addClass('loading');
+  };
+  reader.onload = function(e) {
+    lines = this.result.split('\n');
+    getEmails(lines);
+  }
+  reader.onloadend = function(e) {
+    $('.progress-bar').css('width', '100%').attr('aria-valuenow', 100);
+    $('.progress-bar').append("DONE!");
+    setTimeout("$('.progress-bar').removeClass('loading');", 2000);
+  }
+
+  // Read in the image file as a binary string.
+  reader.readAsText(evt.target.files[0]);
+}
+function getEmails(lines) {
+  var from_emails = new Object();
+  var to_emails = new Object();
+  for(var line = 0; line < lines.length; line++){
+    if (lines[line].match(/From:/)) {
+      var from_line = lines[line].split(/\s+/);
+      var from_test = from_line.shift();
+      if (from_test !== "From:") 
+        continue;
+      var email = from_line.pop();
+      var person = from_line.join(" ");
+      if (typeof(from_emails[email]) === "undefined")
+        from_emails[email] = person;
+    } else if (/To:/) {
+      var to_line = lines[line].split(/\s+/);
+      var to_test = to_line.shift();
+      if (to_test !== "To:") 
+        continue;
+      var email = to_line.pop();
+      var person = to_line.join(" ");
+      if (typeof(to_emails[email]) === "undefined")
+        to_emails[email] = person;
+    } 
+  } 
+  console.log(from_emails);
+  $(".from_emails").append('<p> All "From" emails. Choose which ones for your chatbot');
+  Object.keys(from_emails).forEach(function(key) {
+    console.log(from_emails[key]);
+    var eml = key.replace("<","&lt;");
+    eml = eml.replace(">","&gt;");
+    $(".from_emails").append('<input type="checkbox" name="email" value="' + from_emails[key] + '"> ' + eml + '&nbsp;&nbsp;&nbsp;&nbsp;- name ' + from_emails[key] + ' <br>');
+  });
+  $(".from_emails").append('<input type="submit">');
+}
+
+document.getElementById('files').addEventListener('change', handleFileSelect, false);
